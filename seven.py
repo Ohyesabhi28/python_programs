e9e9eedeed51cb068d8b4aa774fd7929008736ce
abhinav <abhinavnair2nt5@gmail.com>
2025-12-26 12:29:29 +0530

    Adding the python files

---
+++ seven.py
@@ -0,0 +1,10 @@
+sandwich = input("Enter sandwich type (Chicken / Beef / Veg): ")
+
+if sandwich == "Chicken":
+    print("Price: 120")
+elif sandwich == "Beef":
+    print("Price: 150")
+elif sandwich == "Veg":
+    print("Price: 100")
+else:
+    print("Sorry, this sandwich is not available in the menu")
