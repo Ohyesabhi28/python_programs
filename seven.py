f699426dc4aac2427551773181b1e00fb424835d
Ohyesabhi28 <143160525+Ohyesabhi28@users.noreply.github.com>
2025-12-26 12:51:33 +0530

    using switch

--- seven.py
+++ seven.py
@@ -1,10 +1,11 @@
 sandwich = input("Enter sandwich type (Chicken / Beef / Veg): ")
 
-if sandwich == "Chicken":
-    print("Price: 120")
-elif sandwich == "Beef":
-    print("Price: 150")
-elif sandwich == "Veg":
-    print("Price: 100")
-else:
-    print("Sorry, this sandwich is not available in the menu")
+match sandwich:
+    case "Chicken":
+        print("Price: 120")
+    case "Beef":
+        print("Price: 150")
+    case "Veg":
+        print("Price: 100")
+    case _:
+        print("Sorry, this sandwich is not available in the menu")
